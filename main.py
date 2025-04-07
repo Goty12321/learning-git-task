6077785ef84017f9f40e7ae5421a4fc7b30a88a8
Rafał Bastek <rafun2014@gmail.com>
2025-04-07 12:58:00 +0200

    Add Product count

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ lista_zakupów = {
     "piekarnia": ['chleb','pączek','bułki'],
     "warzywniak": ['marchew','seler','rukola']
 }
-
+ilość_produktów = 0
 for key in lista_zakupów:
     values = lista_zakupów[key]
+    ilość_produktów = ilość_produktów + len(values)
     updated_values = []
     for value in values:
         updated_values.append(value.capitalize())
     print(f"Idę do {key.capitalize()} i kupuję tam {updated_values}.")
+print(f"W sumie kupuję {ilość_produktów} produktów.")
